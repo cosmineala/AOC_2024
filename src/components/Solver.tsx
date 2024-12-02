27e1efb56d255bf7c0c3d014e70a79f4b1b215ac
Capisizu Cosmin <cosmineala@example.com>
2024-12-02 22:33:16 +0000

    Partial rework solver factory

--- src/components/Solver.tsx
+++ src/components/Solver.tsx
@@ -23,16 +23,11 @@ const Solver: React.FC<IProps> = ({ day, part }) => {
 
     // const input = use(CreateInput(day));
 
-    let [solver, setSolver] = useState( createSolver(day, part));
-
-    console.log(solver);
-
-
     return (
         <div>
             <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => { setRes(solver.solve(input.current)) }}
+                onClick={() => { setRes(createSolver(day, part).solve(input.current)) }}
             >
                 Solve {day}.{part}
             </button>
--- src/factories/solverFactory.ts
+++ src/factories/solverFactory.ts
@@ -1,19 +1,23 @@
 import Solve_2_1 from "@/daySolvers/Solve_2_1";
 import Solve_2_2 from "@/daySolvers/Solve_2_2";
+import Solve_3_1 from "@/daySolvers/Solve_3_1";
+import Solve_3_2 from "@/daySolvers/Solve_3_2";
 import SolveDay1Part1 from "@/daySolvers/SolveDay1Part1";
 import SolveDay1Part2 from "@/daySolvers/SolveDay1Part2";
 import ISolver from "@/Interfaces/ISolver";
 
 export default function createSolver( day: number, part: number ): ISolver {
-    return solvers.get(`${day}_${part}`) as ISolver;
+    return (solvers.get(`${day}_${part}`)as () => ISolver)(); 
 }
 
-export function registerSolver( day: number, part: number, solver: ISolver ) {
+export function registerSolver( day: number, part: number, solver: () => ISolver ) {
     solvers.set(`${day}_${part}`, solver);
 }
 
-const solvers = new Map<string, ISolver>();
-registerSolver(1, 1, new SolveDay1Part1());
-registerSolver(1, 2, new SolveDay1Part2());
-registerSolver(2, 1, new Solve_2_1());
-registerSolver(2, 2, new Solve_2_2());
+const solvers = new Map<string, () => ISolver>();
+registerSolver(1, 1, () => new SolveDay1Part1());
+registerSolver(1, 2, () => new SolveDay1Part2());
+registerSolver(2, 1, () => new Solve_2_1());
+registerSolver(2, 2, () => new Solve_2_2());
+registerSolver(3, 1, () => new Solve_3_1());
+registerSolver(3, 2, () => new Solve_3_2());
