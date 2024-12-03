86b696c59bcb8cef6b4c192205c3ec93a04f0dd7
Capisizu Cosmin <cosmineala@example.com>
2024-12-03 05:27:55 +0000

    Day 3 raw

--- src/daySolvers/Solve_3_1.ts
+++ src/daySolvers/Solve_3_1.ts
@@ -3,30 +3,34 @@ import ISolver from "@/Interfaces/ISolver";
 
 export default class Solve_3_1 implements ISolver {
 
-    time = 0;
+    time = 0; // 00:07:58 ->  3482
 
     solve(input: string): string {
 
-
+        let sum = 0;
         let lines = input.split("\n")
 
         lines: for (let i = 0; i < lines.length; i++) {
             let line = lines[i];
 
-
-            let elements = line.split(' ').filter((num) => num != '');
-            let nums = elements.map((num) => parseInt(num)).filter((num) => !isNaN(num));
-
-            elements: for (let i = 0; i < nums.length - 1; i++) {
-                let n = nums[i];
-
-
-               
+            // find all matches of mul(44,46) and get first and last number
+            let matches = line.match(/mul\((\d+),(\d+)\)/g);
+            if (matches) {
+                for (let j = 0; j < matches.length; j++) {
+                    // console.log(matches[j]);
+                    let match = matches[j];
+                    let nums = match.match(/(\d+)/g);
+                    let n1 = parseInt(nums[0]);
+                    let n2 = parseInt(nums[1]);
+                    console.log(n1, n2);
+                    let result = n1 * n2;
+                    sum += result;
+                }
             }
-
-
+            
         }
-
+        return sum.toString();
+            
     }
 
     getTime(): string {
--- src/daySolvers/Solve_3_2.ts
+++ src/daySolvers/Solve_3_2.ts
@@ -2,28 +2,45 @@ import ISolver from "@/Interfaces/ISolver";
 
 export default class Solve_3_2 implements ISolver {
 
-    time = 0;
+    time = 0; // 00:12:52 -> 3630 ( 00:12:52 - 00:07:58 = 00:04:54)
 
     solve(input: string): string {
 
+        let sum = 0;
         let lines = input.split("\n")
+        let doMul = true;
 
         lines: for (let i = 0; i < lines.length; i++) {
             let line = lines[i];
 
-
-            let elements = line.split(' ').filter((num) => num != '');
-            let nums = elements.map((num) => parseInt(num)).filter((num) => !isNaN(num));
-
-            elements: for (let i = 0; i < nums.length - 1; i++) {
-                let n = nums[i];
-
-
-               
+            // find all matches of mul(44,46) and get first and last number
+            let matches = line.match(/(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))/g);
+            if (matches) {
+                for (let j = 0; j < matches.length; j++) {
+                    console.log(matches[j]);
+                    if (matches[j] == "don't()") {
+                        doMul = false;
+                        continue;
+                    }
+                    if (matches[j] == 'do()') {
+                        doMul = true;
+                        continue;
+                    }
+                    if (doMul) {
+                        let match = matches[j];
+                        console.log(match);
+                        let nums = match.match(/(\d+)/g);
+                        let n1 = parseInt(nums[0]);
+                        let n2 = parseInt(nums[1]);
+                        let result = n1 * n2;
+                        sum += result;
+                    }
+                }
             }
 
-
+            
         }
+        return sum.toString();
 
 
     }
