4ea6be9b2b241d8ddec620ff52c864a431941ac9
Capisizu Cosmin <cosmineala@example.com>
2024-12-04 08:15:15 +0000

    Day 4 raw

--- src/daySolvers/Solve_4_1.ts
+++ src/daySolvers/Solve_4_1.ts
@@ -7,33 +7,80 @@ export default class Solve_4_1 implements ISolver {
 
     solve(input: string): string {
 
+        let sum = 0 ;
+
         let lines = input.split("\n")
+        let height = lines.length;
+        let width = lines[0].length;
+        // let matrix = lines.map(x => x.split(""));
 
-        lines: for (let i = 0; i < lines.length; i++) {
-            let line = lines[i];
+        // check lines
 
-            // let items = line.split(" ").filter(x => x != "");
-            // items: for (let j = 0; j < items.length; j++) {
-            //     let item = items[j];
-            
-            // }
-
-            // let digits = line.match(/\d+/g);
-            // for (let j = 0; j < digits.length; j++) {
-            //     let digit = digits[j];
-
-            // }
-
-            // let entries = line.matchAll(/(\w+)/g);
-            // for( let entry of entries) {
-            //     let [match, g1, g2] = entry;
-
-            // }
-            
+        function countRowMax( matrix: string[][] ) {
+            for(let i = 0; i < matrix.length; i++) {
+                let row = matrix[i].join("");
+                let matches = row.match(/XMAS/g);
+                if (matches) {
+                    sum += matches?.length;
+                }
+                // mirror
+                matches = row.split("").reverse().join("").match(/XMAS/g);
+                if (matches) {
+                    sum += matches?.length;
+                }
+            }
         }
 
-        return `${input} + Day 4.1 not solved`;
-            
+        let mat = lines.map(x => x.split(""));
+
+        countRowMax(mat); // lines
+
+        // check columns
+        // go thrue matrix and invert lines and columns
+        let newMat = [];
+        for (let i = 0; i < height; i++) {
+            let row = [];
+            for (let j = 0; j < width; j++) {
+                row.push(mat[j][i]);
+            }
+            newMat.push(row);
+        }
+        console.log('invMax',newMat);
+        countRowMax(newMat);
+        
+
+        function diagonal(data: string[][], fromBottom: boolean, direction: string) {
+            const length = { x: data[0].length, y: data.length };
+            const returnMe: string[][] = [];
+        
+            for (let k = 0; k <= length.x + length.y - 2; ++k) {
+                const temp: string[] = [];
+                for (let y = length.y - 1; y >= 0; --y) {
+                    let x;
+                    if (direction === "/") {
+                        x = k - (fromBottom ? length.y - y : y);
+                    } else {
+                        x = k - (fromBottom ? y : length.y - y);
+                    }
+                    if (x >= 0 && x < length.x) {
+                        temp.push(data[y][x]);
+                    }
+                }
+                if (temp.length > 0) {
+                    returnMe.push(temp);
+                }
+            }
+            return returnMe;
+        }
+
+        console.log(mat);
+        console.log(diagonal(mat, false, "/"));
+        console.log(diagonal(mat, false, "asdasd"));
+
+        countRowMax(diagonal(mat, false, "/"));
+        countRowMax(diagonal(mat, false, "other"));
+
+        return sum.toString();
     }
 
     getTime(): string {
--- src/daySolvers/Solve_4_2.ts
+++ src/daySolvers/Solve_4_2.ts
@@ -10,25 +10,46 @@ export default class Solve_4_2 implements ISolver {
         let lines = input.split("\n")
 
         lines: for (let i = 0; i < lines.length; i++) {
-            let line = lines[i];
+            let sum = 0 ;
 
-            // let items = line.split(" ").filter(x => x != "");
-            // items: for (let j = 0; j < items.length; j++) {
-            //     let item = items[j];
+            let lines = input.split("\n")
+            let size = lines.length;
+            // let width = lines[0].length;
+            // let matrix = lines.map(x => x.split(""));
+            let matrix = lines.map(x => x.split(""));
+
+
+            function checkIfIsXformMas( i: number, j: number) {
+                //
+                if( i  <  1 || j < 1 || i > size - 2 || j > size - 2) {
+                    return false;
+                }
+                
+                // get diagonal neighbors in all directions in a 2x2 matrix
+                let neighbors = 
+                    matrix[i-1][j-1]+ matrix[i-1][j+1]+
+                    matrix[i+1][j-1]+ matrix[i+1][j+1];
+
+                let posiblemas = ['MSMS','SMSM','SSMM','MMSS'];
+
+                return posiblemas.includes(neighbors);
+            }
+
+            // iterate matrix
+            for(let i = 0; i < size; i++) {
+                for(let j = 0; j < size; j++) {
+                    if ( matrix[i][j] == 'A' && checkIfIsXformMas(i,j)) {
+                        sum++;
+                        console.log(`${matrix[i][j]} - ${i},${j}\n${matrix[i-1][j-1]}${matrix[i-1][j+1]}\n${matrix[i+1][j-1]}${matrix[i+1][j+1]}`);
+                    }
+                }
+            }
+    
             
-            // }
+    
+            return sum.toString();
 
-            // let digits = line.match(/\d+/g);
-            // for (let j = 0; j < digits.length; j++) {
-            //     let digit = digits[j];
-
-            // }
-
-            // let entries = line.matchAll(/(\w+)/g);
-            // for( let entry of entries) {
-            //     let [match, g1, g2] = entry;
-
-            // }
+            // 2147 too high // 1825 to low
             
         }
 
@@ -40,3 +61,31 @@ export default class Solve_4_2 implements ISolver {
         return this.time + 'ms';
     }
 }
+
+/*
+
+. M . S . . . . . .   M M M S X X M A S M
+. . A . . M S M S .   M S A M X M S M S A
+. M . S . M A A . .   A M X S X M A A M M
+. . A . A S M S M .   M S A M A S M S M X
+. M . S . M . . . .   X M A S A M X A M M
+. . . . . . . . . .   X X A M M X X A M A
+S . S . S . S . S .   S M S M S A S X S S
+. A . A . A . A . .   S A X A M A S A A A
+M . M . M . M . M .   M A M M M X M M M M
+. . . . . . . . . .   M X M X A X M A S X
+
+
+MS
+MS
+
+SM
+SM
+
+SS
+MM
+
+MM
+SS
+
+*/
