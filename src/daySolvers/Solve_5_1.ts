dec4af03c41ccd5ce0432fb04fce980abef1caae
Capisizu Cosmin <cosmineala@example.com>
2024-12-05 05:55:12 +0000

    Day 5 raw

--- src/daySolvers/Solve_5_1.ts
+++ src/daySolvers/Solve_5_1.ts
@@ -1,4 +1,5 @@
 import ISolver from "@/Interfaces/ISolver";
+import { debug } from "console";
 
 export default class Solve_5_1 implements ISolver {
 
@@ -6,15 +7,53 @@ export default class Solve_5_1 implements ISolver {
 
     solve(input: string): string {
 
+        let sum = 0;
+
         let lines = input.split("\n")
+
+        let rules: number[][] = [];
+        let updates: number[][] = [];
+
 
         lines: for (let i = 0; i < lines.length; i++) {
             let line = lines[i];
-          
+
+            if(line.length < 3){
+                continue lines;
+            }
+
+            if(line.length < 7){
+                rules.push(line.split("|").map(x => parseInt(x)));
+                continue lines;
+            }
+            
+            updates.push(line.split(",").map(x => parseInt(x)));
+
             
         }
 
-        return `${input} + Day 5.1 not solved`;
+        update: for (let i = 0; i < updates.length; i++) {
+            let update = updates[i];
+
+            rule: for (let j = 0; j < rules.length; j++) {
+                let rule = rules[j];
+
+                
+                let i1 = update.indexOf(rule[0]);
+                let i2 = update.indexOf(rule[1]);
+                if(i1 > -1 && i2 > -1 && i1 > i2){
+                   continue update;
+               }
+                
+            }
+
+            // add to sum de midle value of update
+            let newLineMid =update[  (update.length - 1) / 2];
+            sum += newLineMid;
+         
+        }
+
+        return sum.toString();
             
     }
 
--- src/daySolvers/Solve_5_2.ts
+++ src/daySolvers/Solve_5_2.ts
@@ -6,16 +6,84 @@ export default class Solve_5_2 implements ISolver {
 
     solve(input: string): string {
 
+        let sum = 0;
+
         let lines = input.split("\n")
+
+        let rules: number[][] = [];
+        let updates: number[][] = [];
+
 
         lines: for (let i = 0; i < lines.length; i++) {
             let line = lines[i];
-          
-            
+
+            if (line.length < 3) {
+                continue lines;
+            }
+
+            if (line.length < 7) {
+                rules.push(line.split("|").map(x => parseInt(x)));
+                continue lines;
+            }
+
+            updates.push(line.split(",").map(x => parseInt(x)));
+
+
         }
 
-        return `${input} + Day 5.2 not solved`;
-            
+
+        function fixBadUpdate(update: number[]) {
+
+            let isBad = true;
+
+            while (isBad) {
+                isBad = false;
+                rule: for (let j = 0; j < rules.length; j++) {
+                    let rule = rules[j];
+
+                    let i1 = update.indexOf(rule[0]);
+                    let i2 = update.indexOf(rule[1]);
+                    if (i1 > -1 && i2 > -1 && i1 > i2) {
+                        isBad = true;
+
+                        // move rule[0] after rule[1]
+                        update.splice(i1, 1);
+                        update.splice(i2, 0, rule[0]);
+
+                    }
+
+                }
+            }
+        }
+
+
+        update: for (let i = 0; i < updates.length; i++) {
+            let update = updates[i];
+            let isBad = false;
+            let isFixed = false;
+
+            rule: for (let j = 0; j < rules.length; j++) {
+                let rule = rules[j];
+
+
+                let i1 = update.indexOf(rule[0]);
+                let i2 = update.indexOf(rule[1]);
+                if (i1 > -1 && i2 > -1 && i1 > i2) {
+                    isBad = true;
+                    fixBadUpdate(update);
+                }
+
+
+            }
+
+            if (isBad) {
+                let newLineMid = update[(update.length - 1) / 2];
+                sum += newLineMid;
+            }
+
+        }
+
+        return sum.toString();
     }
 
     getTime(): string {
