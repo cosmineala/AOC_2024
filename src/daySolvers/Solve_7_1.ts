a3aed898d29f3bf79356dbbc63f6a1bac514d3de
Capisizu Cosmin <cosmineala@example.com>
2024-12-06 20:48:10 +0000

    Prepare for Day 7

---
+++ src/daySolvers/Solve_7_1.ts
@@ -0,0 +1,34 @@
+import ISolver from "@/Interfaces/ISolver";
+
+export default class Solve_7_1 implements ISolver {
+
+    time = 0;
+
+    solve(input: string): string {
+
+        // < v > ^
+        let sum = 0;
+
+        let lines = input.split("\n");
+        line: for (let i = 0; i < lines.length; i++) {
+            let line = lines[i];
+          
+        }
+
+        let matrix = input.split("\n").map(x => x.split(""));
+        line: for (let i = 0; i < matrix.length; i++) {
+            let line = matrix[i];
+            element: for (let j = 0; j < line.length; j++) {
+                let element = line[j];
+                
+            }
+        }
+        
+
+        return input + " solver day 7.1";
+    }
+
+    getTime(): string {
+        return this.time + 'ms';
+    }
+}
