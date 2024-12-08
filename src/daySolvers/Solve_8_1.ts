ae3aae8617388df0e2b01aabd6f83b2d1b3d9a16
Capisizu Cosmin <cosmineala@example.com>
2024-12-08 07:07:17 +0000

    Day 8 raw

--- src/daySolvers/Solve_8_1.ts
+++ src/daySolvers/Solve_8_1.ts
@@ -1,4 +1,5 @@
 import ISolver from "@/Interfaces/ISolver";
+import { debug } from "console";
 
 export default class Solve_8_1 implements ISolver {
 
@@ -7,16 +8,63 @@ export default class Solve_8_1 implements ISolver {
     solve(input: string): string {
 
         let sum = 0;
+        let antenasPos = new Map<string, number[][]>();
 
-        let lines = input.split("\n");
+        let matrix = input.split("\n").map(x => x.split(""));
+        let newMatrix = structuredClone(matrix);
 
-        line: for (let i = 0; i < lines.length; i++) {
-            let line = lines[i];
-          
+        line: for (let i = 0; i < matrix.length; i++) {
+            let line = matrix[i];
+            element: for (let j = 0; j < line.length; j++) {
+                let element = line[j];
+
+                if (element === '.') continue element;
+
+                if (antenasPos.has(element)) {
+                    let antenas = antenasPos.get(element) as number[][];
+                    antenas.push([i, j]);
+                    antenasPos.set(element, antenas);
+                } else {
+                    antenasPos.set(element, [[i, j]]);
+                }
+
+            }
 
         }
 
-        return input + ' solver day 8.1';
+        antenaFrequency: for (let [key, value] of antenasPos) {
+            antenaPos: for (let pos of value) {
+                let [x, y] = pos;
+                otherAntenasPos: for (let value2 of value) {
+                    let [x2, y2] = value2;
+                    if (x === x2 && y == y2) continue otherAntenasPos;
+                    
+                    let antinodesPos = [ x + (x - x2), y + (y - y2) ];
+                    if (antinodesPos[0] === 0 && antinodesPos[1] === 6){
+                        debugger;
+                    };
+                    if (
+                        antinodesPos[0] < 0 || antinodesPos[0] >= matrix.length ||
+                        antinodesPos[1] < 0 || antinodesPos[1] >= matrix.length
+                    )
+                        continue otherAntenasPos;
+                    
+                    newMatrix[antinodesPos[0]][antinodesPos[1]] = '#';
+                }
+
+            }
+        }
+
+        console.log(newMatrix.map(x => x.join("")).join("\n"));
+
+        for (let line of newMatrix) {
+            for (let element of line) {
+                if (element === '#') sum++;
+            }
+        }
+
+
+        return sum.toString();
     }
 
     getTime(): string {
--- src/daySolvers/Solve_8_2.ts
+++ src/daySolvers/Solve_8_2.ts
@@ -7,16 +7,69 @@ export default class Solve_8_2 implements ISolver {
     solve(input: string): string {
 
         let sum = 0;
+        let antenasPos = new Map<string, number[][]>();
 
-        let lines = input.split("\n");
+        let matrix = input.split("\n").map(x => x.split(""));
+        let newMatrix = structuredClone(matrix);
 
-        line: for (let i = 0; i < lines.length; i++) {
-            let line = lines[i];
-          
+        line: for (let i = 0; i < matrix.length; i++) {
+            let line = matrix[i];
+            element: for (let j = 0; j < line.length; j++) {
+                let element = line[j];
+
+                if (element === '.') continue element;
+
+                if (antenasPos.has(element)) {
+                    let antenas = antenasPos.get(element) as number[][];
+                    antenas.push([i, j]);
+                    antenasPos.set(element, antenas);
+                } else {
+                    antenasPos.set(element, [[i, j]]);
+                }
+
+            }
 
         }
 
-        return input + ' solver day 8.2';
+        antenaFrequency: for (let [key, value] of antenasPos) {
+            antenaPos: for (let pos of value) {
+                let [x, y] = pos;
+                otherAntenasPos: for (let value2 of value) {
+                    let [x2, y2] = value2;
+                    if (x === x2 && y == y2) continue otherAntenasPos;
+
+                    let opx = x - x2;
+                    let opy = y - y2;
+                    let multiplier = 1;
+
+                    while (true) {
+
+                        let antinodesPos = [x + opx * multiplier, y + opy * multiplier];
+
+                        if (
+                            antinodesPos[0] < 0 || antinodesPos[0] >= matrix.length ||
+                            antinodesPos[1] < 0 || antinodesPos[1] >= matrix.length
+                        )
+                            continue otherAntenasPos;
+
+                        newMatrix[antinodesPos[0]][antinodesPos[1]] = '#';
+                        multiplier++;
+                    }
+                }
+
+            }
+        }
+
+        console.log(newMatrix.map(x => x.join("")).join("\n"));
+
+        for (let line of newMatrix) {
+            for (let element of line) {
+                if (element !== '.') sum++;
+            }
+        }
+
+        return sum.toString();
+
     }
 
     getTime(): string {
