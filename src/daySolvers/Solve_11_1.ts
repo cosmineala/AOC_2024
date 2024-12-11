3a41801806cccbcf3e4618122c88be1488eb89f0
cosmineala <cosmineala@gmail.com>
2024-12-11 21:15:58 +0000

    Day 11 raw

--- src/daySolvers/Solve_11_1.ts
+++ src/daySolvers/Solve_11_1.ts
@@ -6,22 +6,42 @@ export default class Solve_11_1 implements ISolver {
 
     solve(input: string): string {
 
-       
+        let stones = input.split(" ").map(x => parseInt(x));
 
         let sum = 0;
-        let mtx = input.split("\n").map(x => x.split("").map(x => parseInt(x)));
-        const size = mtx.length;
+        let endTimes = 25;
 
-        lines: for (let i = 0; i < size; i++) {
-            elements: for (let j = 0; j < size; j++) {
-                let element = mtx[i][j];
+        function blink(stone: number, times: number): number {
 
+            if (times == 0) {
+                return 1;
             }
+            times--;
+
+            if (stone === 0) {
+                return blink(1, times);
+            }
+
+            if (stone.toString().length % 2 === 0) {
+                let half = stone.toString().length / 2;
+                let firstHalf = parseInt(stone.toString().substring(0, half));
+                let secondHalf = parseInt(stone.toString().substring(half));
+
+                return blink(firstHalf, times) + blink(secondHalf, times);
+            }
+
+            return blink(stone * 2024, times);
+
         }
 
-        
-        return sum.toString() + " Solve 11.1 " + input;
+        sum = stones.reduce((acc, stone, index) => {
+            console.log(acc);
+            return acc + blink(stone, endTimes);
+        }, 0);
+
+        return sum.toString();
     }
+
 
     getTime(): string {
         return this.time + 'ms';
--- src/daySolvers/Solve_11_2.ts
+++ src/daySolvers/Solve_11_2.ts
@@ -6,21 +6,57 @@ export default class Solve_11_2 implements ISolver {
 
     solve(input: string): string {
 
-       
+        let stones = input.split(" ").map(x => parseInt(x));
 
         let sum = 0;
-        let mtx = input.split("\n").map(x => x.split("").map(x => parseInt(x)));
-        const size = mtx.length;
+        let endTimes = 75;
+        let map = new Map<string, number>();
 
-        lines: for (let i = 0; i < size; i++) {
-            elements: for (let j = 0; j < size; j++) {
-                let element = mtx[i][j];
+        function blink(stone: number, times: number): number {
 
+            if (times == 0) {
+                return 1;
             }
+            times--;
+
+            let value = 0;
+            let k = `${stone}_${times}`;
+
+            switch (true){
+
+                case map.has(k):
+                    value = map.get(k) as number;
+                    break;
+
+                case stone === 0:
+                    value = blink(1, times);
+                    map.set(k, value);
+                    break;
+
+                case stone.toString().length % 2 === 0:
+
+                    let half = stone.toString().length / 2;
+                    let firstHalf = parseInt(stone.toString().substring(0, half));
+                    let secondHalf = parseInt(stone.toString().substring(half));
+
+                    value = blink(firstHalf, times) + blink(secondHalf, times);
+                    map.set(k, value);
+                    break;
+                    
+                    default:
+                        value = blink(stone * 2024, times);
+                        map.set(k, value);
+            }
+
+            return value
         }
 
-        
-        return sum.toString() + " Solve 11.2 " + input;
+        sum = stones.reduce((acc, stone, index) => {
+            return acc + blink(stone, endTimes);
+        }, 0);
+
+       
+        return sum.toString();
     }
 
     getTime(): string {
